9a29f00f3d139ef760f2ac565a088de5038dbff8
DHKim0428 <sidsince0428@gmail.com>
2018-04-23 22:55:46 +0900

    Prettify

--- Parsing.py
+++ Parsing.py
@@ -3,13 +3,14 @@ from bs4 import BeautifulSoup
 
 def main():
     filename = str(input('file name: '))
-    f = open(filename, 'r')
+    f = open(filename, 'r', encoding="UTF8")
     data = f.read()
     f.close()
-    f = open(filename, 'w+')
+    f = open(filename, 'w+', encoding="UTF8")
     soup = BeautifulSoup(data, 'lxml-xml')
     data = soup.prettify()
     f.write(data)
+    print(data)
 
 
 main()
